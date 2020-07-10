7f8336cc53b4f7e43fee679646e3f9d68fe92d6c
Arvind Kumar <arvind.cs12@gmail.com>
2020-07-10 23:52:43 +0530

    added controllers for owner and vet

---
+++ pet-clinic-web/src/main/java/com/example/arv/controller/VetController.java
@@ -0,0 +1,14 @@
+package com.example.arv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/vets")
+@Controller
+public class VetController {
+
+	@RequestMapping({ "", "/", "/index", "/index.html" })
+	public String listVets() {
+		return "vet/index";
+	}
+}
